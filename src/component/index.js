13a7bc36e78e08c398ba013744e3468727231a6d
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-13 14:26:14 +0530

    basic set up

---
+++ src/component/index.js
@@ -0,0 +1,2 @@
+export { Navbar } from './navbar';
+export { Drawer } from './drawer';
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return(
+    <div className="video-list-wrapper d-flex flex-col">
+      <div className="category-wrapper d-flex"></div>
+      <div className="sort-wrapper"></div>
+      <div className="video-list">
+      </div>
+    </div>
+  )   
+}
+export {Home}
