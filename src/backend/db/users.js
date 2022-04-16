111e33289901cdbc28c9d60679fbda987689bbe1
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-16 10:31:32 +0530

    dummy user added

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshBalika123",
+    firstName: "John",
+    lastName: "Doe",
+    email: "johndoe@gmail.com",
+    password: "johndoe123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
