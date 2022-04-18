d4e9edd1e8a1539d6121b9fa76a0ca88c42c6850
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-18 18:05:02 +0530

    services and api calls for like and dislike videos

--- src/service/index.js
+++ src/service/index.js
@@ -1,4 +1,5 @@
 import { getAllVideos, getVideo } from './video-service';
+import { addItemToLikedVideos, deleteItemFromLikedVideos } from './like-service';
 import { getAllCategories } from './category-service';
 import { loginUser, signupUser } from './auth-service';
-export { getAllVideos, getVideo, getAllCategories, loginUser, signupUser };
+export { getAllVideos, getVideo, getAllCategories, loginUser, signupUser, addItemToLikedVideos, deleteItemFromLikedVideos };
