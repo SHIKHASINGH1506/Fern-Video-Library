16a2d4fd1ae858a4ee636b3dea0de57b58c86fcc
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-19 12:04:42 +0530

    added private routes

--- src/route/ProtectedRoute.jsx
+++ src/route/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  //const isAuth = localStorage.getItem("isAuth");
-  return (
-    <main>
+  const location = useLocation();
+  const isAuth = localStorage.getItem("isAuth");
+  return isAuth ? 
+    (<main>
       <Outlet />
-    </main>
-  );
+    </main>)
+    : (<Navigate to="/login" replace state={{from : location.pathname}} />);
 };
 
 export { ProtectedRoute };
