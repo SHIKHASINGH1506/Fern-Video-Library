e6c7cc285e5be8039dc8aae5fe90550d91be595f
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-19 21:39:26 +0530

    import export

--- src/service/index.js
+++ src/service/index.js
@@ -2,4 +2,5 @@ import { getAllVideos, getVideo } from './video-service';
 import { addItemToLikedVideos, deleteItemFromLikedVideos } from './like-service';
 import { getAllCategories } from './category-service';
 import { loginUser, signupUser } from './auth-service';
-export { getAllVideos, getVideo, getAllCategories, loginUser, signupUser, addItemToLikedVideos, deleteItemFromLikedVideos };
+import { addItemToWatchLater, removeItemFromWatchLater } from './watch-later-service';
+export { getAllVideos, getVideo, getAllCategories, loginUser, signupUser, addItemToLikedVideos, deleteItemFromLikedVideos, addItemToWatchLater, removeItemFromWatchLater };
