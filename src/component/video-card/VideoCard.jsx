6241610a1a67562a89e5bc1b1dd092913a5e7924
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-19 21:39:58 +0530

    feat: add vidoe to watch later, remove video from watch later

--- src/component/video-card/VideoCard.jsx
+++ src/component/video-card/VideoCard.jsx
@@ -2,17 +2,66 @@ import './video-card.css';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import WatchLaterOutlinedIcon from '@mui/icons-material/WatchLaterOutlined';
 import PlayCircleOutlineOutlinedIcon from '@mui/icons-material/PlayCircleOutlineOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
+import { addItemToWatchLater, removeItemFromWatchLater } from 'service';
+import { useData, useAuth } from 'context';
+import { useToast } from 'custom-hook/useToast';
+
 const VideoCard = ({video}) => {
   const navigate = useNavigate();
+  const { videoDispatch, videoState:{watchlater}} = useData();
+  const { auth:{isAuth} } = useAuth();
+  const { showToast } = useToast();
   const [optionModal, setOptionalModal] = useState(false);
   const optionHandler = e =>{
     e.stopPropagation();
     setOptionalModal(prevModalState => !prevModalState);
   }
+  const isVideoInWatchLater = () => watchlater.find(item => item._id === video._id) ? true : false;
+  const watchLaterIcon = isVideoInWatchLater() ? <DeleteOutlineOutlinedIcon style={{fontSize: "1rem"}}/> : <WatchLaterOutlinedIcon style={{fontSize: "1rem"}}/>;
+
+  const addToWatchLater = async (video) => {
+    try{
+      const{data:{watchlater}} = await addItemToWatchLater(video);
+      videoDispatch({
+        type: 'SET_WATCH_LATER', 
+        payload: {
+          watchlater:watchlater
+        }
+      });
+      showToast('Video added to watch later', 'success');
+    }catch(error){
+      showToast('Could not add to watch later!', 'error');
+      console.log(error.response.data);
+    }
+  }
+  const removeFromWatchLater = async (id) => {
+    try{
+      const{data:{watchlater}} = await removeItemFromWatchLater(id);
+      videoDispatch({
+        type: 'SET_WATCH_LATER', 
+        payload: {
+          watchlater:watchlater
+        }
+      });
+      showToast('Video removed from watch later', 'success');
+    }catch(error){
+      showToast('Could not remove from watch later!', 'error');
+      console.log(error.response.data);
+    }
+  }
+
+  const watchLaterHandler = (e) => {
+    e.stopPropagation();
+    isAuth
+      ? isVideoInWatchLater() ? removeFromWatchLater(video._id) : addToWatchLater({video: video})
+      : navigate('/login', {replace: true, state:{from : location.pathname}});
+  }
+  
   return (
     <div className="card" key={video._id} onClick={() => navigate(`/video/${video._id}`)}>
       <div className="img__container">
@@ -23,9 +72,9 @@ const VideoCard = ({video}) => {
         <MoreVertOutlinedIcon onClick={e => optionHandler(e)}/>
       </div>
      {optionModal && <div className="option-modal">
-        <div className="option d-flex items-center">
-          <WatchLaterOutlinedIcon style={{fontSize: "1rem"}}/>
-          Add to Watch Later
+        <div className="option d-flex items-center" onClick={e => watchLaterHandler(e)}>
+          {watchLaterIcon}
+          {isVideoInWatchLater() ? 'Remove from Watch Later' : 'Add to Watch Later'}
         </div>
         <div className="option d-flex items-center">
           <PlayCircleOutlineOutlinedIcon style={{fontSize: "1rem"}}/>
