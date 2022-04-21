eaa4aed9602da7b1872cabb07456041472331504
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-21 13:20:44 +0530

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export { Signup } from './auth/signup';
 export { SingleVideo } from './single-video';
 export { LikedVideos } from './likes';
 export { WatchLater } from './watch-later';
+export { History } from './history';
