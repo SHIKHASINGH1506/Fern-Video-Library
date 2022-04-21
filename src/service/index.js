7b88923c467c1fb553b5fdd921fb034362c93a14
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-21 18:47:36 +0530

    import export refactored

--- src/service/index.js
+++ src/service/index.js
@@ -1,7 +1,18 @@
-import { getAllVideos, getVideo } from './video-service';
-import { addItemToLikedVideos, deleteItemFromLikedVideos } from './like-service';
-import { getAllCategories } from './category-service';
-import { loginUser, signupUser } from './auth-service';
-import { getWatchLaterItems, addItemToWatchLater, removeItemFromWatchLater } from './watch-later-service';
-import { getHistoryVideos, addVideoToHistory, removeVideoFromHistory, removeAllVideoFromHistory } from './history-service';
-export { getAllVideos, getVideo, getAllCategories, loginUser, signupUser, addItemToLikedVideos, deleteItemFromLikedVideos, addItemToWatchLater, removeItemFromWatchLater,getWatchLaterItems, getHistoryVideos, addVideoToHistory, removeVideoFromHistory, removeAllVideoFromHistory };
+export { getAllVideos, getVideo } from './video-service';
+export { 
+    addItemToLikedVideos, 
+    deleteItemFromLikedVideos 
+} from './like-service';
+export { getAllCategories } from './category-service';
+export { loginUser, signupUser } from './auth-service';
+export { 
+    getWatchLaterItems, 
+    addItemToWatchLater, 
+    removeItemFromWatchLater 
+} from './watch-later-service';
+export { 
+    getHistoryVideos, 
+    addVideoToHistory, 
+    removeVideoFromHistory, 
+    removeAllVideoFromHistory 
+} from './history-service';
